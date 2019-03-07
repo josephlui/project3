03046db87a96a664a23010973e29a7bf37497685
Joseph Lui <joseph.lui2000@gmail.com>
2019-03-06 22:47:41 -0500

    Revert "more testing"
    This reverts commit 8cf69517bb288b5cab4931cd3a397458227bbfd7.

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          Testing 2...
+          Testing....
         </p>
       </div>
     );
