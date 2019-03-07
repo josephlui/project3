8cf69517bb288b5cab4931cd3a397458227bbfd7
Joseph Lui <joseph.lui2000@gmail.com>
2019-03-06 22:34:39 -0500

    more testing

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          Testing....
+          Testing 2...
         </p>
       </div>
     );
