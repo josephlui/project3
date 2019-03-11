371539313c7a6f8056594611b9aa3610d73261e5
Judy Hu <judy.hu9@gmail.com>
2019-03-10 22:16:38 -0400

    followed Joseph's instructions for components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,11 +3,15 @@ import { BrowserRouter, Route } from "react-router-dom";
 
 import Login from "./Auth/Login";
 import Register from "./Auth/Register";
+import Navbar from "./common/Navbar";
+import Hero from "./main/Hero";
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
+        <Navbar />
+        <Hero />
         <Route path="/Login" exact component={Login} />
         <Route path="/Register" exact component={Register} />
       </BrowserRouter>
