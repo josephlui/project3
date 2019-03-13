b7fc34047b18c93c2780c8d48af6f7e00e9505a4
Joseph Lui <joseph.lui2000@gmail.com>
2019-03-13 09:37:08 -0400

    update sample auto search bar to profile.js

--- client/src/components/pages/Profile.js
+++ client/src/components/pages/Profile.js
@@ -1,8 +1,50 @@
 import React from "react";
+import Autocomplete from "../common/util/AutoComplete";
+import API from "../common/util/API";
+import _ from "lodash";
 
 class Profile extends React.Component {
+  
+  state = {
+    userList: [],
+    connectionRequestList: []
+  };
+
+  componentDidMount() {
+    API.retrieveAllUsers()
+    .then( result => {
+       if (result.status === 200) {
+        const userIds = result.data.map (user => user.userId);
+        this.setState({userList: userIds });
+       }
+    })
+  }
+
+  onSelect = clientId => {
+
+    this.setState((state, props) => {
+      const tmpList = _.filter(state.userList, function(list) { return list !== clientId; });
+      return {
+        connectionRequestList : [...state.connectionRequestList,clientId],
+        userList: tmpList
+      }
+    })
+  };
+
   render() {
-    return <div>Profile page</div>;
+   
+    return <div>Profile page
+      
+      Sample code to lookup all user IDs in user collection:
+      <br/>
+      
+      <Autocomplete
+        suggestions={this.state.userList}
+        onSelect={this.onSelect}
+      />
+      Connections: {this.state.connectionRequestList.join()}
+
+    </div>;
   }
 }
 
