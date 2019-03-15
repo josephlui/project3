11d400d1e12d8b5019a2d90b160f0dae35ad290d
Joseph Lui <joseph.lui2000@gmail.com>
2019-03-14 22:21:44 -0400

    base calendar view from react calendar

--- client/src/components/pages/Appointment.js
+++ client/src/components/pages/Appointment.js
@@ -1,4 +1,7 @@
-import React from "react";
+import React from 'react';
+import { ReactAgenda , ReactAgendaCtrl , guid ,  Modal } from 'react-agenda';
+
+require('moment/locale/fr.js')
 
 class Appointment extends React.Component {
   render() {
