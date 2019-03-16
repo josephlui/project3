0dc925de720575a727c8a8b17461870d7ff52f6d
Judy Hu <judy.hu9@gmail.com>
2019-03-16 12:35:47 -0400

    nav bar links

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,4 @@
 import React from "react";
-// import { Route, Switch } from "react-router-dom";
-
-// import Login from "./Auth/Login";
-// import Register from "./Auth/Register";
 import Navbar from "./common/Navbar";
 import Hero from "./main/Hero";
 import Exemplar from "./main/Exemplar";
@@ -10,20 +6,26 @@ import Features from "./main/Features";
 import About from "./main/About";
 import SocialBrand from "./main/SocialBrand";
 import Footer from "./common/Footer";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Login from "./Auth/Login";
+import Register from "./Auth/Register";
+import Appointments from "./Appointments/Appointments";
+import Connections from "./Connections/Connections";
 
 const App = () => {
   return (
     <div id="app">
       <Navbar />
-      <Hero />
-      <Exemplar />
-      <Features />
-      <About />
-
-      <SocialBrand />
+      <Route path="/" exact component={Hero} />
+      <Route path="/" exact component={Exemplar} />
+      <Route path="/" exact component={Features} />
+      <Route path="/" exact component={About} />
+      <Route path="/" exact component={SocialBrand} />
+      <Route path="/appointments" exact component={Appointments} />
+      <Route path="/connections" exact component={Connections} />
+      <Route path="/login" exact component={Login} />
+      <Route path="/register" exact component={Register} />
       <Footer />
-      {/* <Route path="/Login" exact component={Login} />
-      <Route path="/Register" exact component={Register} /> */}
     </div>
   );
 };
