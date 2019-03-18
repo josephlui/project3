b5da643d056c6394a388652265ffda615828956a
Judy Hu <judy.hu9@gmail.com>
2019-03-18 11:27:03 -0400

    fixed mobile view

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -6,8 +6,7 @@ import Features from "./pages/main/Features";
 import About from "./pages/main/About";
 import SocialBrand from "./pages/main/SocialBrand";
 import Footer from "./pages/main/Footer";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-// Ignore the error that tells you to delete the Router, bc the page crashes when it gets deleted
+import { Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Logout from "./pages/Logout";
 import Register from "./Auth/Register";
--- client/src/components/pages/main/SocialBrand.js
+++ client/src/components/pages/main/SocialBrand.js
@@ -1,13 +1,14 @@
 import React from "react";
-// import logo from "./logo.png";
+import logo from "./logo.png";
 
 const SocialBrand = () => {
   return (
     <section id="social" className="grey lighten-2">
       <div className="container center-align">
         <h1>
-          <a href="#about" className="brand-logo" />
-          {/* <Image source={logo} /> */}
+          <a href="#about" className="brand-logo">
+            <img className="" src={logo} alt="logo" />
+          </a>
         </h1>
       </div>
     </section>
